d37457fa1455cebdbc1b440888e08103d312f076
f.hance <f.hance@SHRL2018W001.PARROT.BIZ>
2019-02-18 19:29:16 +0100

    Itération 01: maj

--- view/view_reservation.php
+++ view/view_reservation.php
@@ -99,28 +99,48 @@
 
                 </tr>
                 </thead>
-                <?php foreach ($books as $book) : ?>
+  -                <?php foreach($rentals as $rental): ?>
++                <?php foreach($selections as $selection): ?>
+                     <tr>
+-                        <td><?= $rental->isbn ?></td>
+-                        <td><?= $rental->title ?></td>
+-                        <td><?= $rental->author ?></td>
+-                        <td><?= $rental->editor ?></td>
+-                        <td><?= $rental->picture ?></td>
++                        <td><?= $selection->isbn ?></td>
++                        <td><?= $selection->title ?></td>
++                        <td><?= $selection->author ?></td>
++                        <td><?= $selection->editor ?></td>
++                        <td><?= $selection->picture ?></td>
+                         <td>
+                             <form  action='book/details' method='post'>
+-                                <input type='hidden' name='details' value='<?= $rental->id ?>'>
++                                <input type='hidden' name='details' value='<?= $selection->id ?>'>
+                                 <input type='submit' value='details'>
+                             </form>
+                         </td>
+                         <td>
+-                            <form action='book/selection' method='post'>
+-                                <input type='hidden' name='selection' value='<?= $rental->id ?>' >
++                            <form action='book/book_selection' method='post'>
++                                <input type='hidden' name='selection' value='<?= $selection->id ?>' >
+                                 <input type='submit' value='selection'>
+                             </form>
+                         </td>
+                     </tr>       
+                 <?php endforeach; ?>
 
-                    <tr>
-                        <td><?= $book->isbn ?></td>
-                        <td><?= $book->title ?></td>
-                        <td><?= $book->author ?></td>
-                        <td><?= $book->editor ?></td>
-                        <td><?= $book->picture ?></td>
-                        <td>
-                            <form  action='book/details' method='post'>
-                                <input type='hidden' name='details' value='<?= $book->id ?>'>
-                                <input type='submit' value='details'>
-                            </form>
                         </td>
-                        <td>
-                            <form   action='book/selection' method='post'>
-                                <input type='hidden' name='selection' value=''<?= $book->id ?> >
-                                <input type='submit' value='selection'>
-                            </form>
-
-                        </td>
-                    </tr>       
+                          <?php if ($user->isManager($user->username)): ?>
+                    <form  action='book/details' method='post'>
+                        <input type='hidden' name='details' value='<?= $book->id ?>'>
+                        <input type='submit' value='details'>
+                    </form>
+                          <?php endif; ?>
+                    </td>
+                    </form>
+                    </td>
+                    <td>   
 
                 <?php endforeach; ?>
 
