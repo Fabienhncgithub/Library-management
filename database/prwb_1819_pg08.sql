6d6fc375efc6e1089145685eaaa8cd200f1df0da
f.hance <f.hance@SHRL2018W001.PARROT.BIZ>
2019-02-18 18:45:05 +0100

    Itération 01: Note database

--- database/prwb_1819_pg08.sql
+++ database/prwb_1819_pg08.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
--- https://www.phpmyadmin.net/
+-- version 4.5.4.1
+-- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Dec 16, 2018 at 02:47 PM
--- Server version: 10.1.36-MariaDB
--- PHP Version: 7.2.11
+-- Client :  localhost
+-- Généré le :  Lun 18 Février 2019 à 17:43
+-- Version du serveur :  5.7.11
+-- Version de PHP :  7.0.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -19,79 +17,133 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `prwb_1819_pg08`
+-- Base de données :  `prwb_1819_pg08`
 --
-DROP DATABASE IF EXISTS `prwb_1819_pg08`;
 CREATE DATABASE IF NOT EXISTS `prwb_1819_pg08` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `prwb_1819_pg08`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `book`
+-- Structure de la table `book`
 --
 
-DROP TABLE IF EXISTS `book`;
-CREATE TABLE IF NOT EXISTS `book` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `book` (
+  `id` int(11) NOT NULL,
   `isbn` char(13) NOT NULL,
   `title` varchar(255) NOT NULL,
   `author` varchar(255) NOT NULL,
   `editor` varchar(255) NOT NULL,
-  `picture` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `isbn_UNIQUE` (`isbn`)
+  `picture` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `book`
+--
+
+INSERT INTO `book` (`id`, `isbn`, `title`, `author`, `editor`, `picture`) VALUES
+(1, '1234567891234', 'coucou ', 'salut', 'hey', NULL),
+(3, '2234567891234', 'azerty', 'qwerty', 'pocket', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `rental`
+-- Structure de la table `rental`
 --
 
-DROP TABLE IF EXISTS `rental`;
-CREATE TABLE IF NOT EXISTS `rental` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `rental` (
+  `id` int(11) NOT NULL,
   `user` int(11) NOT NULL,
   `book` int(11) NOT NULL,
   `rentaldate` datetime DEFAULT NULL,
-  `returndate` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_rentalitem_book1_idx` (`book`),
-  KEY `fk_rentalitem_user1_idx` (`user`)
+  `returndate` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Structure de la table `user`
 --
 
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
   `username` varchar(32) NOT NULL,
   `password` varchar(255) NOT NULL,
   `fullname` varchar(255) NOT NULL,
   `email` varchar(64) NOT NULL,
   `birthdate` date DEFAULT NULL,
-  `role` enum('admin','manager','member') NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username_unique` (`username`) USING BTREE,
-  UNIQUE KEY `email_unique` (`email`)
+  `role` enum('admin','manager','member') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Constraints for dumped tables
+-- Contenu de la table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `fullname`, `email`, `birthdate`, `role`) VALUES
+(1, 'admin', 'c6aa01bd261e501b1fea93c41fe46dc7', 'Administrateur', 'admin@test.com', NULL, 'admin'),
+(2, 'ben', 'cc4902e2506fc6de54e53489314c615a', 'Benoît', 'ben@test.com', '1999-01-01', 'manager'),
+(3, 'test', '6833f56bb07f8df131d6c97cc6587129', 'Testeur', 'test@test.com', NULL, 'member'),
+(4, 'Fabien1', 'cfc22bce25c416e6bddb277393aa7e12', '0', '0', '0000-00-00', 'member'),
+(8, 'Fabien2', '74ed877d96ce494ebad26b835bbbe089', 'Fabien H', 'fabien@sky-hero.com', '0000-00-00', 'manager'),
+(14, 'Fabien3', 'f0cb425d8629e0167c97b45c802e2672', 'Fabien H', 'piperley@hotmail.com', '2019-01-10', 'member'),
+(15, 'Fabien4', 'f10a4fe70d807b3b0177ee85c3696c22', 'fabien H', 'Fabien4@hotmail.com', '2019-02-14', 'admin');
+
+--
+-- Index pour les tables exportées
 --
 
 --
--- Constraints for table `rental`
+-- Index pour la table `book`
+--
+ALTER TABLE `book`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `isbn_UNIQUE` (`isbn`);
+
+--
+-- Index pour la table `rental`
+--
+ALTER TABLE `rental`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_rentalitem_book1_idx` (`book`),
+  ADD KEY `fk_rentalitem_user1_idx` (`user`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username_unique` (`username`) USING BTREE,
+  ADD UNIQUE KEY `email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `book`
+--
+ALTER TABLE `book`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT pour la table `rental`
+--
+ALTER TABLE `rental`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `rental`
 --
 ALTER TABLE `rental`
   ADD CONSTRAINT `fk_rentalitem_book` FOREIGN KEY (`book`) REFERENCES `book` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_rentalitem_user1` FOREIGN KEY (`user`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
