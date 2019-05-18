85c0f87aaaf5a8182557b9a5f33a616fbdf21b32
f.hance <f.hance@SHRL2018W001.PARROT.BIZ>
2019-05-18 20:46:30 +0200

    verification form add user

--- view/view_add-user.php
+++ view/view_add-user.php
@@ -8,6 +8,101 @@
         <link href="css/styles.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
+          
+  <script src="lib/jquery-3.3.1.min.js" type="text/javascript"></script>
+        <script src="lib/jquery-validation-1.19.0/jquery.validate.min.js" type="text/javascript"></script>
+        <script>
+            $.validator.addMethod("regex", function (value, element, pattern) {
+                if (pattern instanceof Array) {
+                    for(p of pattern) {
+                        if (!p.test(value))
+                            return false;
+                    }
+                    return true;
+                } else {
+                    return pattern.test(value);
+                }
+            }, "Please enter a valid input.");
+            $(function(){
+                $('#adduserForm').validate({
+                    rules: {
+                        username: {
+                            remote: {
+                                url: 'user/user_exists_service',
+                                type: 'post',
+                                data:  {
+                                    username: function() { 
+                                        return $("#username").val();
+                                    }
+                                }
+                            },
+                            required: true,
+                            minlength: 3,
+                            maxlength: 16,
+                            regex: /^[a-zA-Z][a-zA-Z0-9]*$/,
+                        },
+                        password: {
+                            required: true,
+                            minlength: 8,
+                            maxlength: 16,
+                            regex: [/[A-Z]/, /\d/, /['";:,.\/?\\-]/],
+                        },
+                        password_confirm: {
+                            required: true,
+                            minlength: 8,
+                            maxlength: 16,
+                            equalTo: "#password",
+                            regex: [/[A-Z]/, /\d/, /['";:,.\/?\\-]/],
+                        },
+                        fullname: {
+                             required: true,
+                        },
+                        email: {
+                               remote: {
+                                url: 'user/email_exists_service',
+                                type: 'post',
+                                data:  {
+                                    username: function() { 
+                                        return $("#email").val();
+                                    }
+                                }
+                            },
+                             required: true,
+                        }
+                    },
+                    messages: {
+                        username: {
+                            remote: 'this username is already taken',
+                            required: 'required',
+                            minlength: 'minimum 3 characters',
+                            maxlength: 'maximum 16 characters',
+                            regex: 'bad format for username',
+                        },
+                        password: {
+                            required: 'required',
+                            minlength: 'minimum 8 characters',
+                            maxlength: 'maximum 16 characters',
+                            regex: 'bad password format',
+                        },
+                        password_confirm: {
+                            required: 'required',
+                            minlength: 'minimum 8 characters',
+                            maxlength: 'maximum 16 characters',
+                            equalTo: 'must be identical to password above',
+                            regex: 'bad password format',
+                        },
+                          fullname: {
+                               required: 'required', 
+                        },
+                        email:{
+                            remote: 'this email is already taken',
+                            required: 'required', 
+                        }
+                    }
+                });
+                $("input:text:first").focus();
+            });
+        </script>
         <div class="title">Add User</div>
        
         
@@ -17,15 +112,17 @@
         <div class="main">
             Please enter the user details :
             <br><br>
-            <form id="adduser" action="user/adduser" method="post">
+            <form id="adduserForm" action="user/adduser" method="post">
                 <table>
                     <tr>
                         <td>User Name:</td>
                         <td><input id="username" name="username" type="text" value="<?php echo $username; ?>"></td>
+                        <td class="errors" id="errPseudo"></td>
                     </tr>
                     <tr>
                         <td>Full Name:</td>
                         <td><input id="fullname" name="fullname" type="text" value="<?php echo $fullname; ?>"></td>
+                          <td class="errors" id="errPassword"></td>
                     </tr>
                     <tr>
                         <td>Password:</td>
@@ -40,10 +137,12 @@
                     <tr>
                         <td>Email:</td>
                         <td><input id="email" name="email" type="email" value="<?php echo $email; ?>"></td>
+                             <td class="errors" id="errEmail"></td>
                     </tr>
                     <tr>
                         <td>Birth Date:</td>
                         <td><input id="birthdate" name="birthdate" type="date" value="<?php echo $birthdate; ?>"></td>
+                            <td class="errors" id="errBirthdate"></td>
                     </tr>
                     
                              <tr>
